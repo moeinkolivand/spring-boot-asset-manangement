3d039a938e7dcf27e4301b2981c1123d707abc97
moein <moeinkolivand97@gmail.com>
2026-07-20 03:45:10 +0330

    add login response,userprofile dto

---
+++ src/main/java/com/example/demo/user/LoginResponseDto.java
@@ -0,0 +1,8 @@
+package com.example.demo.user;
+
+
+public record LoginResponseDto(
+        String token
+) {
+}
+
---
+++ src/main/java/com/example/demo/user/UserProfileDto.java
@@ -0,0 +1,8 @@
+package com.example.demo.user;
+
+public record UserProfileDto(
+        String name,
+        String lastName,
+        String phoneNumber
+) {
+}
