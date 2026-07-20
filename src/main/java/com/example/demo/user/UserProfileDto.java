8f0731edae69d87115cb4af88d9de2137a7f811e
moein <moeinkolivand97@gmail.com>
2026-07-20 15:04:18 +0330

    make the user profile dto accept null values

--- src/main/java/com/example/demo/user/UserProfileDto.java
+++ src/main/java/com/example/demo/user/UserProfileDto.java
@@ -1,8 +1,12 @@
 package com.example.demo.user;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserProfileDto(
-        String name,
-        String lastName,
-        String phoneNumber
-) {
-}
+        @Size(min = 3, max = 35) String name,
+        @Size(min = 3, max = 35) String lastName,
+        @Size(min = 11, max = 11) @Pattern(regexp = "^\\d{11}$", message = "Must be exactly 11 digits") String phoneNumber
+) { }
