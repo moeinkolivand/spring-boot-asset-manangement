3201d2381471ff115126d3f47d55715521f90179
moein <moeinkolivand97@gmail.com>
2026-07-22 13:37:13 +0330

    add method findByIdempotencyKey into TransactionRepository

--- src/main/java/com/example/demo/transaction/TransactionRepository.java
+++ src/main/java/com/example/demo/transaction/TransactionRepository.java
@@ -1,6 +1,11 @@
 package com.example.demo.transaction;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Optional<Transaction> findByIdempotencyKey(@NotBlank(message = "idempotencyKey is required") UUID uuid);
 }
