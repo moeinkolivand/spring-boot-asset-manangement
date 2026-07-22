558b0dbad2d21cc8ca907c2a07ae30e153866522
moein <moeinkolivand97@gmail.com>
2026-07-22 13:37:27 +0330

    just formatting

--- src/main/java/com/example/demo/wallet/WalletRepository.java
+++ src/main/java/com/example/demo/wallet/WalletRepository.java
@@ -2,15 +2,13 @@ package com.example.demo.wallet;
 
 import com.example.demo.currency.Currency;
 import com.example.demo.user.User;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
-
     boolean existsByUserAndCurrency(User user, Currency currency);
-
     List<Wallet> findByUser(User user);
-
+    Optional<Wallet> findByCurrencyNameAndUser(String currencyName, User user);
 }
