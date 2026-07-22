e225a3821a81082ac0f890a8371a5d95e899359a
moein <moeinkolivand97@gmail.com>
2026-07-22 17:49:44 +0330

    add findByIdForUpdate for locking the wallet with pessimistic_write lock mode

--- src/main/java/com/example/demo/wallet/WalletRepository.java
+++ src/main/java/com/example/demo/wallet/WalletRepository.java
@@ -2,13 +2,22 @@ package com.example.demo.wallet;
 
 import com.example.demo.currency.Currency;
 import com.example.demo.user.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
     boolean existsByUserAndCurrency(User user, Currency currency);
+    boolean existsByUserAndCurrencyName(User user, String currencyName);
     List<Wallet> findByUser(User user);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT w FROM Wallet w WHERE w.id = :id")
+    Optional<Wallet> findByIdForUpdate(@Param("id") Long id);
     Optional<Wallet> findByCurrencyNameAndUser(String currencyName, User user);
 }
